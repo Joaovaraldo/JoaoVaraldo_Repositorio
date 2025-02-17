f49ef710300611adbf8b29efee73e35cf5c0bb8e
Joaovaraldo <joao_varaldo@estudante.sesisenai.org.br>
2025-02-17 15:22:36 -0300

    update

---
+++ python/funcao11.py
@@ -0,0 +1,7 @@
+def Lequantidade():
+    num = input("Digite um numero: ")  
+    quantidade_de_numeros = len(num)  
+    return quantidade_de_numeros
+
+resultado = Lequantidade()  
+print(resultado)  
